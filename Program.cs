e6c7ca88ff5686c7622ea7887de4d7ac1cbc70c6
Marcin Dziedzic <marcin.dziedzic49@microsoft.wsei.edu.pl>
2022-03-16 22:31:58 +0100

    tests added?

--- Program.cs
+++ Program.cs
@@ -8,22 +8,22 @@ namespace Lab_1
         {
             //https://dirask.com/posts/WSEI-2021-2022-lato-labN-1-ISN-Programowanie-obiektowe-lab-1-pa3ek1 zadanie xD
             fraction a = new fraction(2, 3);
-            fraction b = new fraction(2, 6);
+            fraction b = new fraction(2, 3);
             fraction d = new fraction(4, 2);
             fraction c = a + b;
 
-            fraction[] fractarray = new fraction [4] { a, b,c,d };
-
-            bool x;
-            x = fractarray[0].Equals(fractarray[1]);
-            Console.WriteLine(x);
+            //fraction[] fractarray = new fraction [4] { a, b,c,d };
+            //Console.WriteLine(a.CompareTo(b)+"reeee");
+            //bool x;
+            //x = fractarray[0].Equals(fractarray[1]);
+            //Console.WriteLine(x);
             
             //Console.WriteLine(a);
             //Console.WriteLine(a.ToString());
 
 
 
-            Console.WriteLine(c);
+            Console.WriteLine(c.ToString());
 
 
             Console.WriteLine("test");
